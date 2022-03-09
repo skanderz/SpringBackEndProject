9c9f6efb3c12a9ddd412e0876cc8206125e9c44b
skanderz <49171432+skanderz@users.noreply.github.com>
2022-03-10 00:17:28 +0100

    Add files via upload

---
+++ src/main/java/tn/spring/repositories/SubscriptionRepository.java
@@ -0,0 +1,45 @@
+package tn.esprit.spring.repository;
+
+import java.util.Date;
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import tn.esprit.spring.entity.Subscription;
+
+@Repository
+public interface SubscriptionRepository extends CrudRepository <Subscription, Long> ,JpaRepository <Subscription, Long>{
+	
+
+	
+	@Transactional
+	@Modifying
+	@Query("update Subscription s set s.dateFin= :dateFin where s.idSub= :idSub")
+	int ExtendSubscription(@Param("idSub") Long idSub ,@Param("dateFin") Date dateFin);
+	
+	
+	@Transactional
+	@Modifying
+	@Query("update Subscription s set s.dateFin= :dateFin where s.idSub= :idSub")
+	int  ChangeToPremium(@Param("idSub") Long idSub ,@Param("dateFin") Date dateFin);
+	
+	@Transactional
+	@Modifying
+	@Query("update Subscription s set s.typeSub='Basic' where s.idSub= :idSub")
+	int  ChangeToBasic(@Param("idSub") Long idSub);
+	
+	
+	
+	
+	 
+	 
+	
+	
+	
+
+}
+
---
+++ src/main/java/tn/spring/repositories/UserRepository.java
@@ -0,0 +1,60 @@
+package tn.esprit.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entity.ERole;
+import tn.esprit.spring.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> ,CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByIduser(int iduser);
+    List<User> findByStatus(boolean etat);
+    Boolean existsByUsername(String id); 
+    Optional<ERole> findByRole(ERole role);
+    
+    
+    @Transactional
+	@Modifying
+	@Query("update User u set u.status=FALSE where u.iduser= :iduser")
+	int DisableAccount(@Param("iduser") Long iduser);
+    
+    
+    @Transactional
+ 	@Modifying
+ 	@Query("update User u set u.status=FALSE where u.iduser= :iduser")
+ 	int EnableAccount(@Param("iduser") Long iduser);
+	
+    
+    @Transactional
+ 	@Modifying
+ 	@Query("update User u set u.password=:password where u.iduser= :iduser")
+ 	int ChangePassword(@Param("iduser") Long iduser ,@Param("password") String password);
+    
+    
+    @Transactional
+ 	@Modifying
+ 	@Query("update User u set u.solde=:solde where u.iduser= :iduser")
+ 	int AddSolde(@Param("iduser") Long iduser ,@Param("solde") double solde);
+    
+    
+    
+    
+    
+    
+    
+
+	
+    
+    
+}
